6d398b0529d8dabdd7b3baff63b52fbb9a01bf3c
Abdulbaki Dursun <eggsydev@outlook.com>
2020-08-28 01:57:44 +0300

    :fire: remove used account record when a verified user leaves

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -8,6 +8,7 @@ export default class MemberLeaveEvent extends Event {
   async execute(guild: Guild, member: Member, bot: Bot) {
     if (bot.master.queue.has(`${guild.id}/${member.id}`)) {
       bot.master.queue.delete(`${guild.id}/${member.id}`);
+
       if (bot.settings.logChannel.id) {
         bot.settings.logChannel.channel.createMessage(
           bot.master.strings.bot.info["USER_LEFT_SERVER"]
@@ -15,6 +16,16 @@ export default class MemberLeaveEvent extends Event {
             .replace(/\{1\}/g, member.id)
         );
       }
+    }
+
+    const userIndex: number = bot.master.usedAccounts.findIndex((a) =>
+      a.includes(`${member.id}/`)
+    );
+
+    if (userIndex !== -1) {
+      bot.master.usedAccounts = bot.master.usedAccounts.filter(
+        (a: string, i: number) => i !== userIndex
+      );
     }
   }
 }
