959041177dfea793db4c39ef525236c66fb7ee90
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-05 21:53:58 +0300

    feat: use tsup as the bundler

---
+++ tsup.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "tsup";
+import { config } from "dotenv";
+import { ChildProcess, spawn } from "child_process";
+import { cp } from "fs";
+
+config();
+
+let cProcess: ChildProcess;
+
+export default defineConfig({
+  entry: ["src/**/*.ts"],
+  watch: process.env.NODE_ENV !== "production",
+  clean: true,
+  env: {
+    BOT_TOKEN: process.env.BOT_TOKEN,
+  },
+  loader: {
+    ".handlebars": "file",
+  },
+  async onSuccess() {
+    cp(
+      "src/views",
+      "dist/views",
+      {
+        recursive: true,
+      },
+      console.error
+    );
+
+    if (process.env.NODE_ENV === "production") return;
+
+    if (cProcess?.killed === false) {
+      cProcess.kill();
+      cProcess = null;
+    }
+
+    cProcess = spawn("node", ["dist/index.js"], {
+      stdio: "inherit",
+    });
+  },
+});
