804ac0fd47492f559af95e124c212b08646a767e
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-07 23:04:40 +0300

    chore: dont check if account is used

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -8,20 +8,22 @@ export default class NewMemberEvent extends Event {
 
   async execute(guild: Guild, member: Member, bot: Bot) {
     const memberRoles: string[] = [];
-    if (member && member.roles && member.roles.length)
-      bot.settings.successRoles.forEach((r) =>
-        member.roles.includes(r) ? memberRoles.push(r) : null
-      );
+
+    if (member?.roles)
+      bot.settings.successRoles.forEach((r) => {
+        if (member.roles.includes(r)) memberRoles.push(r);
+      });
 
     if (
       !bot.settings.enabled ||
       memberRoles.length === bot.settings.successRoles.length ||
-      bot.master.queue.has(`${guild.id}/${member.id}`)
+      bot.master.queue.has(`${guild.id}/${member.id}`) ||
+      bot.master.usedAccounts.includes(`${guild.id}/${member.id}`)
     )
       return;
 
     try {
-      bot.startVerification(guild, member);
+      await bot.startVerification(guild, member);
     } catch (err) {
       consola.error(err);
     }
