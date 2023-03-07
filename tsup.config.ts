d37f75d39613a42be357d03b37ed4b9e15368672
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-07 17:47:29 +0300

    chore: minify on production

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,37 +8,39 @@ config();
 
 let cProcess: ChildProcess;
 
-export default defineConfig({
-  entry: ["src/**/*.ts"],
-  watch: process.env.NODE_ENV !== "production",
-  clean: true,
-  env: {
-    BOT_TOKEN: process.env.BOT_TOKEN,
-  },
-  loader: {
-    ".handlebars": "file",
-  },
-  async onSuccess() {
-    cp(
-      "src/views",
-      "dist/views",
-      {
-        recursive: true,
-      },
-      (err) => {
-        if (err) consola.error(err);
+export default defineConfig((options) => {
+  return {
+    minify: !options.watch,
+    entry: ["src/**/*.ts"],
+    clean: true,
+    env: {
+      BOT_TOKEN: process.env.BOT_TOKEN,
+    },
+    loader: {
+      ".handlebars": "file",
+    },
+    async onSuccess() {
+      cp(
+        "src/views",
+        "dist/views",
+        {
+          recursive: true,
+        },
+        (err) => {
+          if (err) consola.error(err);
+        }
+      );
+
+      if (process.env.NODE_ENV === "production") return;
+
+      if (cProcess?.killed === false) {
+        cProcess.kill();
+        cProcess = null;
       }
-    );
 
-    if (process.env.NODE_ENV === "production") return;
-
-    if (cProcess?.killed === false) {
-      cProcess.kill();
-      cProcess = null;
-    }
-
-    cProcess = spawn("node", ["dist/index.js"], {
-      stdio: "inherit",
-    });
-  },
+      cProcess = spawn("node", ["dist/index.js"], {
+        stdio: "inherit",
+      });
+    },
+  };
 });
